53a59697bc4a8212f1c35c3f286bb33891022ec3
muriloschneider <murilosch31@gmail.com>
2022-05-15 09:59:42 -0300

    teste

---
+++ conf/conf.inc.php
@@ -0,0 +1,14 @@
+<?php
+    // Banco de Dados
+    define('HOST', 'localhost');  
+    define('DBNAME', 'provaprog2');    
+    define('USER', 'root');  
+    define('PASSWORD', '');
+
+    define('DRIVER', 'mysql'); 
+    define('CHARSET', 'utf8');
+
+    // Geral da Aplicação
+    define('NOME_DO_PROJETO','provaprog2');
+    define('DESCRICAO_DO_PROJETO','Classes Estado e Cidade');
+?>
