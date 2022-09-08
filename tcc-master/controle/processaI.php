c3f77fa03dc84b0a6a13dba7d3fd8f4e85621497
muriloschneider <murilosch31@gmail.com>
2022-09-08 14:16:54 -0300

    ..

---
+++ tcc-master/controle/processaI.php
@@ -0,0 +1,60 @@
+<?php
+    require_once "../conf/Conexao.php";
+    include_once "../classes/autoload.php";
+
+    $id = isset($_POST['id']) ? $_POST['id'] : 0;   
+    $nome = isset($_POST['nome']) ? $_POST['nome'] : "";
+    $login = isset($_POST['login']) ? $_POST['login'] : ""; 
+    $email = isset($_POST['email']) ? $_POST['email'] : "";   
+    $senha = isset($_POST['senha']) ? $_POST['senha'] : ""; 
+    $sobre = isset($_POST['sobre']) ? $_POST['sobre'] : "";
+    $regiao = isset($_POST['regiao']) ? $_POST['regiao'] : "";
+    $website = isset($_POST['website']) ? $_POST['website'] : "";
+  
+  
+    if(isset($_POST['processo'])){$processo = $_POST['processo'];}else if(isset($_GET['processo'])){$processo = $_GET['processo'];}else{$processo="";}
+
+    if ($processo == "salvar"){
+         //verifica se o ID é igual a 0, se for cria/insere novo usuário, se não edita as informações no banco.
+        if ($id == 0){
+            $user = new Usuario("", $_POST['nome'],  $_POST['login'], $_POST['email'], $_POST['senha'], "", "", "");                 
+            $resultado = $user->inserir();
+            header("location:../php/confirmacaologin.php");
+    
+        } 
+}
+
+
+//Consulta os dados dentro do banco.
+    function buscarDados($id){
+    $pdo = Conexao::getInstance();
+    $consulta = $pdo->query("SELECT * FROM usuario WHERE id = $id");
+    $dados = array();
+    while ($linha = $consulta->fetch(PDO::FETCH_ASSOC)) {
+        $dados['id'] = $linha['id'];
+        $dados['nome'] = $linha['nome'];
+        $dados['login'] = $linha['login'];
+        $dados['email'] = $linha['email'];
+        $dados['senha'] = $linha['senha'];
+        $dados['sobre'] = $linha['sobre'];
+        $dados['regiao'] = $linha['regiao'];
+        $dados['website'] = $linha['website'];
+       
+    }
+    return $dados;
+}
+
+    include_once "../classes/autoload.php";
+    if ($processo == "login"){
+        if(isset($_POST['login']) && isset($_POST['senha']) && Usuario::efetuarLogin($_POST['login'], $_POST['senha'])) {
+            header("Location:../php/principal.php");
+        } else if(isset($_POST['login']) && isset($_POST['senha'])) {
+            header("Location: login.php?msg=Login Não-Efetuado!");
+        }
+    }
+
+
+
+
+
+?>
---
+++ tcc-master/php/confirmacaologin.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Confirmação de Cadastro</title>
+<link rel=" shortcut icon" href="../imagens/favicon.png" type="image/x-icon">   
+<link rel="stylesheet" href="../css/csstcc.css"/>
+
+    </head>
+
+
+    <body class = "confirmacao">
+
+    <div class="stars">
+    <div class="twinkling"> 
+    <div class="clouds"></div>
+            
+ 
+<div class="caixaI">
+<div class="caixaII">
+<div class="caixaIII">
+
+
+<div class="caixaIV">
+
+
+
+<center><p class="titulo"> LOGIN </p></center>
+
+
+<form method="post" action="../controle/processaI.php" >
+<div class="color">
+<br>    
+    
+                <div class="icon">
+                <img class="icon" src="../imagens/user2.png">
+                <input type="text" name="login" id="login" class="form-control" placeholder="Login"><br>                </div>
+
+
+<br>
+                <div class="icon">
+                <img class="icon" src="../imagens/senha.png">      
+                <input type="password" name="senha" id="senha" class="form-control" placeholder="Insira a Senha"><br>
+                </div>
+
+<br>
+<center>
+                    <button name="processo" value="login" id="processo" type="submit">Entrar</button>
+</center>
+</form>
+
+</table>
+</div>
+
+
+<p class="descri">Não tem uma conta ainda?<br> Cadastre-se <a href="cadastro.php">aqui</a></p>
+
+
+
+</div>
+</div>
+
+<footer>
+    <div class="footer">
+    
+        <div class="redesocial">
+        <img class="rede" src="../imagens/instagram.png"><p class="redesocial">in/astrodisseia</p>
+        </div>
+
+        <div class="redesocial">
+        <img class="rede" src="../imagens/facebook.png"><p class="redesocial">fb/astrodisseia</p>
+        </div>
+
+        <div class="redesocial">
+        <img class="rede" src="../imagens/telegram.png"><p class="redesocial">t.me/astrodisseia</p>
+        </div>
+        
+        
+    </div>
+
+
+     <div class="footer1">
+    <div class="footer3">
+    <div class="footer4">
+    <div class="footer5">
+    <img src="../imagens/logo1.png" class="logo">
+    <p class="odi">ODISSEIA</p>
+    </div>
+    </div>
+    </div>
+    </div>
+</footer>
+    <div class="preto">
+</div>
+<div class="aviso" style="margin-top: 50%;">
+    <center><p class = "aviso">CADASTRO EFETUADO COM SUCESSO</p></center>
+    <br>
+    <a href="login.php"><button class = "avancar"  style = "margin-left: 35%;">CONTINUAR</button></a>
+</div>
+    </div>
+    </div>
+    
+</body>
+
+</html>
